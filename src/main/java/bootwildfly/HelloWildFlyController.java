8a0534240058039c3a7839f0f7f6910c806cbc49
Adrian <adrimol@gmail.com>
2018-07-05 11:15:06 -0500

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/bootwildfly/HelloWildFlyController.java
    	src/main/webapp/index.html

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,7 +1,6 @@
 package bootwildfly;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
