b8bdc89cb7f645c24b91173ac03f71e7e7ef4d5b
Adrian <adrimol@gmail.com>
2018-07-06 14:11:24 -0500

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/bootwildfly/HelloWildFlyController.java
    	src/main/webapp/index.html

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
